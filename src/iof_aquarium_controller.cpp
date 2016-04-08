d5b5a4e0fe081b3d6315ed90b8f7c88112aa028e
Dieter Niklaus <dieter.niklaus@contractors.roche.com>
2016-04-08 16:13:25 +0200

    add Free Heap Logger; WiFi & MQTT connection controlled Timer based

--- src/iof_aquarium_controller.cpp
+++ src/iof_aquarium_controller.cpp
@@ -1,22 +1,17 @@
 // Do not remove the include below
 #include "iof_aquarium_controller.h"
 
-#include "FishActuator.h"
-
-#include "CapSensor.h"
-#include "Cmd.h"
-#include "DbgCliNode.h"
-#include "DbgCliCommand.h"
-#include "DbgCliTopic.h"
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-
 #include <Wire.h>
+#include <CapSensor.h>
+#include <ESP8266WiFi.h>
+#include <Timer.h>
+#include <TimerContext.h>
+#include <PubSubClient.h>
+#include <FishActuator.h>
 
 // Update these with values suitable for your network.
-
-#define WIFI_SSID       "IoTAP"
-#define WIFI_PWD        "12345678"
+#define WIFI_SSID       "DNNet"
+#define WIFI_PWD        "Furz1234"
 #define MQTT_SERVER_IP  "iot.eclipse.org"
 #define MQTT_PORT       1883
 
@@ -33,10 +28,95 @@
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-FishActuator* fishActuator;
+FishActuator* fishActuator = 0;
 
 #define SDA_PIN 4
 #define SCL_PIN 5
+
+//-----------------------------------------------------------------------------
+// Free Heap Logger
+//-----------------------------------------------------------------------------
+extern "C"
+{
+  #include "user_interface.h"
+}
+const unsigned long c_freeHeapLogIntervalMillis = 10000;
+class FreeHeapLogTimerAdapter : public TimerAdapter
+{
+public:
+  void timeExpired()
+  {
+    Serial.print(millis() / 1000);
+    Serial.print(" - Free Heap Size: ");
+    Serial.println(system_get_free_heap_size());
+  }
+};
+
+//-----------------------------------------------------------------------------
+// WiFi Connection Handler
+//-----------------------------------------------------------------------------
+void subscribe();
+const unsigned long c_wiFiConnectIntervalMillis = 1000;
+class WiFiConnectTimerAdapter : public TimerAdapter
+{
+private:
+  wl_status_t m_wlStatus;
+public:
+  WiFiConnectTimerAdapter()
+  : m_wlStatus(WL_NO_SHIELD)
+  { }
+
+  void timeExpired()
+  {
+    wl_status_t wlStatus = WiFi.status();
+    if (wlStatus != m_wlStatus)
+    {
+      m_wlStatus = wlStatus;
+      Serial.print("WiFi - SSID: ");
+      Serial.print(WIFI_SSID);
+      Serial.print(" - Status: ");
+      Serial.println(m_wlStatus == WL_NO_SHIELD       ? "NO_SHIELD      " :
+                     m_wlStatus == WL_IDLE_STATUS     ? "IDLE_STATUS    " :
+                     m_wlStatus == WL_NO_SSID_AVAIL   ? "NO_SSID_AVAIL  " :
+                     m_wlStatus == WL_SCAN_COMPLETED  ? "SCAN_COMPLETED " :
+                     m_wlStatus == WL_CONNECTED       ? "CONNECTED      " :
+                     m_wlStatus == WL_CONNECT_FAILED  ? "CONNECT_FAILED " :
+                     m_wlStatus == WL_CONNECTION_LOST ? "CONNECTION_LOST" :
+                     m_wlStatus == WL_DISCONNECTED    ? "DISCONNECTED   " : "UNKNOWN");
+    }
+
+    if ((WL_CONNECTED == WiFi.status()) && (!client.connected()))
+    {
+      Serial.print("Attempting MQTT connection... ");
+      // Attempt to connect
+      if (client.connect("iofiof-1"))
+      {
+        Serial.println("connected");
+        delay(5000);
+        // resubscribe
+        subscribe();
+      }
+      else
+      {
+        int state = client.state();
+        Serial.print("failed, state: ");
+        Serial.print(state == MQTT_CONNECTION_TIMEOUT      ? "CONNECTION_TIMEOUT"      :
+                     state == MQTT_CONNECTION_LOST         ? "CONNECTION_LOST"         :
+                     state == MQTT_CONNECT_FAILED          ? "CONNECT_FAILED"          :
+                     state == MQTT_DISCONNECTED            ? "DISCONNECTED"            :
+                     state == MQTT_CONNECTED               ? "CONNECTED"               :
+                     state == MQTT_CONNECT_BAD_PROTOCOL    ? "CONNECT_BAD_PROTOCOL"    :
+                     state == MQTT_CONNECT_BAD_CLIENT_ID   ? "CONNECT_BAD_CLIENT_ID"   :
+                     state == MQTT_CONNECT_UNAVAILABLE     ? "CONNECT_UNAVAILABLE"     :
+                     state == MQTT_CONNECT_BAD_CREDENTIALS ? "CONNECT_BAD_CREDENTIALS" :
+                     state == MQTT_CONNECT_UNAUTHORIZED    ? "CONNECT_UNAUTHORIZED"    : "UNKNOWN");
+        Serial.print(", will try again in ");
+        Serial.print(c_wiFiConnectIntervalMillis / 1000);
+        Serial.println(" second(s)");
+      }
+    }
+  }
+};
 
 //-----------------------------------------------------------------------------
 // Fish Event & Error Notification Adapter
@@ -80,35 +160,21 @@ public:
 
   virtual void notifyCapTouched(uint8_t currentTouchValue)
   {
-    if (0 < (currentTouchValue & 1<<0))
+    if (0 != (currentTouchValue & 1<<0))
     {
       // now it is time to do something
       Serial.println("Touch down!");
       client.publish("iof/ch/berne/sensor/aquarium-trigger", MY_FISH_ID);
     }
-    if (0 < (currentTouchValue & 1<<7))
+    if (0 != (currentTouchValue & 1<<7))
     {
-      m_fishActuator->stopFish();
+      if (0 != m_fishActuator)
+      {
+        m_fishActuator->stopFish();
+      }
     }
   }
 };
-
-//-----------------------------------------------------------------------------
-// Arduino Cmd I/F
-//-----------------------------------------------------------------------------
-void dbgCliExecute(int arg_cnt, char **args)
-{
-  if (0 != DbgCli_Node::RootNode())
-  {
-    const unsigned int firstArgToHandle = 1;
-    DbgCli_Node::RootNode()->execute(static_cast<unsigned int>(arg_cnt), const_cast<const char**>(args), firstArgToHandle);
-  }
-}
-
-void hello(int arg_cnt, char **args)
-{
-  Serial.println("Hello world.");
-}
 
 //-----------------------------------------------------------------------------
 // MQTT Client subscriber callback
@@ -122,63 +188,54 @@ void callback(char* topic, byte* payload, unsigned int length)
   for (int i = 0; i < length; i++)
   {
     Serial.print((char) payload[i]);
+    Serial.print(" ");
   }
-
-  if (NULL == fishActuator)
-  {
-    return;
-  }
-
-  switch (payload[0])
-  {
-    case '1':
-      fishId = 0;
-      break;
-    case '2':
-      fishId = 1;
-      break;
-    case '3':
-      fishId = 2;
-      break;
-  }
-
-  fishActuator->activateFish(fishId);
-
   Serial.println();
-}
 
-//
-void setup_wifi()
-{
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print(F("Connecting to WiFi:"));
-  Serial.println(WIFI_SSID);
-
-  WiFi.begin(WIFI_SSID, WIFI_PWD);
-
-  while (WiFi.status() != WL_CONNECTED)
+  if (NULL != fishActuator)
   {
-    delay(500);
-    Serial.print(".");
+    switch (payload[0])
+    {
+      case '1':
+        fishId = 0;
+        break;
+      case '2':
+        fishId = 1;
+        break;
+      case '3':
+        fishId = 2;
+        break;
+      case '4':
+        fishId = 3;
+        break;
+      case '5':
+        fishId = 4;
+        break;
+      case '6':
+        fishId = 5;
+        break;
+      case '7':
+        fishId = 6;
+        break;
+      case '8':
+        fishId = 7;
+        break;
+    }
+    fishActuator->activateFish(fishId);
   }
-
-  Serial.println("");
-  Serial.println(F("WiFi connected"));
-  Serial.println(F("IP address: "));
-  Serial.println(WiFi.localIP());
-  Serial.println(F("MAC address: "));
-  Serial.println(WiFi.macAddress());
 }
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
+  //-----------------------------------------------------------------------------
+  // Free Heap Logger
+  //-----------------------------------------------------------------------------
+  new Timer(new FreeHeapLogTimerAdapter(), Timer::IS_RECURRING, c_freeHeapLogIntervalMillis);
+
   Wire.begin(SDA_PIN, SCL_PIN);
 
-  cmdInit(115200); //contains Serial.begin(115200);
+  Serial.begin(115200);
 
   Serial.println();
   Serial.println(F("---------------------------------------------"));
@@ -186,8 +243,19 @@ void setup()
   Serial.println(F("---------------------------------------------"));
   Serial.println();
 
-  setup_wifi();
+  //-----------------------------------------------------------------------------
+  // WiFi Connection
+  //-----------------------------------------------------------------------------
+  WiFi.begin(WIFI_SSID, WIFI_PWD);
+  new Timer(new WiFiConnectTimerAdapter(), Timer::IS_RECURRING, c_wiFiConnectIntervalMillis);
 
+  //-----------------------------------------------------------------------------
+  // MQTT Client
+  //-----------------------------------------------------------------------------
+  while (WL_CONNECTED != WiFi.status())
+  {
+    delay(200);
+  }
   client.setServer(MQTT_SERVER_IP, MQTT_PORT);
   client.setCallback(callback);
 
@@ -198,21 +266,8 @@ void setup()
   fishActuator->addFishAtHwId(0);
   fishActuator->addFishAtHwId(1);
   fishActuator->addFishAtHwId(2);
+
   CapSensor* capSensor = new CapSensor(new MyCapSensorAdatper(fishActuator, 0));
-
-  //---------------------------------------------------------------------------
-  // Debug Cli
-  //---------------------------------------------------------------------------
-  // adding CLI Commands
-  cmdAdd("hello", hello);        // Tsst dummy
-  cmdAdd("dbg", dbgCliExecute);  // DbgCli integration
-
-  //---------------------------------------------------------------------------
-  // Debug Trace
-  //---------------------------------------------------------------------------
-//  DbgCli_Topic* traceTopic = new DbgCli_Topic(DbgCli_Node::RootNode(), "tr", "Modify debug trace");
-//  DbgTrace_Context* traceContext = new DbgTrace_Context(traceTopic);
-//  new DbgTrace_Out(DbgTrace_Context::getContext(), "trConOut", new DbgPrint_Console());
 }
 
 void subscribe()
@@ -221,41 +276,9 @@ void subscribe()
   client.subscribe("iof/#");
 }
 
-void reconnect()
-{
-  // Loop until we're reconnected
-  while (!client.connected())
-  {
-    Serial.print(F("Attempting MQTT connection..."));
-    // Attempt to connect
-    if (client.connect(MY_FISH_ID))
-    {
-      Serial.println(F("connected"));
-      // resubscribe
-      subscribe();
-      delay(500);
-    }
-    else
-    {
-      Serial.print(F("failed, rc="));
-      Serial.print(client.state());
-      Serial.println(F(" try again in 5 seconds"));
-      // Wait 3 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
 // The loop function is called in an endless loop
 void loop()
 {
-  if (!client.connected())
-  {
-    reconnect();
-    delay(100);
-  }
-
+  client.loop();
   yield();
-  client.loop(); //must be called regularly
-  cmdPoll();
 }
